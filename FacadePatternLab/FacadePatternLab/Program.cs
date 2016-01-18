7c9fc2da752d944ef8e96b7a9f43d71090b2cb4c
teargone08 <teargone08@yahoo.com>
2016-01-17 22:31:04 -0600

    final-01

---
+++ FacadePatternLab/FacadePatternLab/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadePatternLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShapeMaker shapeMake=new ShapeMaker();
+            shapeMake.DrawCircle();
+            shapeMake.DrawRectangle();
+            shapeMake.DrawSquare();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FacadePatternLab/FacadePatternLab/ShapeMaker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadePatternLab
+{
+    public class ShapeMaker
+    {
+        private Circle circle;
+        private Rectangle rectangle;
+        private Square square;
+        public ShapeMaker()
+        {
+            circle = new Circle();
+            rectangle = new Rectangle();
+            square = new Square();
+        }
+        public void DrawCircle()
+        {
+            circle.Draw();
+        }
+        public void DrawRectangle()
+        {
+            rectangle.Draw();
+        }
+        public void DrawSquare()
+        {
+            square.Draw();
+        }
+    }
+}
